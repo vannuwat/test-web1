4e45b858b3fb32c9bd92087f1816cf91340889a6
vannuwat <114803233+vannuwat@users.noreply.github.com>
2022-11-03 11:00:14 +0700

    Add files via upload

--- src/components/content.js
+++ src/components/content.js
@@ -4,15 +4,118 @@ import React from 'react';
 
 import moment from "moment";
 import Chart from 'chart.js/auto';
-import {Line} from 'react-chartjs-2';
+import {Line, Bar} from 'react-chartjs-2';
 
 class contentPage extends React.Component{ 
 
   constructor(props) {
     super(props);
-    this.state = { dataArr: [], dataArr2: [], dataArr3: []};
+    this.state = { dataArr: [], dataArr2: [], dataArr3: [], dataGraphOv1: [], dataGraphOv2: [], dataGraphOv3: []};
   }
   
+  data_ov1(){
+    const labels = ['Station 1', 'Station 2', 'Station 3', 'Station 4', 'Station 5', 'Station 6', 'Station 7', 'Station 8'];
+    const data_ov_analysis = {
+      labels: labels,
+      datasets: [
+        {
+          label: "Passengers in bus per station",
+          data: this.state.dataGraphOv1,
+          fill: false,
+          backgroundColor: "#FF3871",
+          borderColor: "#FF3871",
+          color: '#FFFFFF'
+        },
+        
+      ]
+    };
+    const options_ov_analysis = {
+      plugins: {  
+        legend: {
+          labels: {
+            color: "white",  
+            // font: {
+            //   size: 10
+            // }
+          }
+        }
+      },
+      scales: {
+        y: {
+          ticks: { color: 'white', beginAtZero: true}
+        },
+        x: {
+          ticks: { color: 'white', beginAtZero: true }
+        }
+
+      }
+    };
+    return(
+      <div className="col-xl-6 col-sm-6 mt-3">
+        <div className="card bg-dark text-white">
+          <div className="card-body">
+            <Line data={data_ov_analysis} options={options_ov_analysis} />  
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  data_ov2(){
+    const labels = ['Station 1', 'Station 2', 'Station 3', 'Station 4', 'Station 5', 'Station 6', 'Station 7', 'Station 8'];
+    const data_ov_analysis2 = {
+      labels: labels,
+      datasets: [
+        {
+          label: "Get In Passengers",
+          data: this.state.dataGraphOv2,
+          fill: false,
+          backgroundColor: "#36A0FF",
+          borderColor: "#36A0FF",
+          color: '#FFFFFF'
+        },
+        {
+          label: "Get Out Passengers",
+          data: this.state.dataGraphOv3,
+          fill: false,
+          backgroundColor: "#36FF42",
+          borderColor: "#36FF42",
+          color: '#FFFFFF'
+        },
+        
+      ]
+    };
+    const options_ov_analysis2 = {
+      plugins: {  
+        legend: {
+          labels: {
+            color: "white",  
+            // font: {
+            //   size: 10
+            // }
+          }
+        }
+      },
+      scales: {
+        y: {
+          ticks: { color: 'white', beginAtZero: true}
+        },
+        x: {
+          ticks: { color: 'white', beginAtZero: true }
+        }
+
+      }
+    };
+    return(
+      <div className="col-xl-6 col-sm-6 mt-3">
+        <div className="card bg-dark text-white">
+          <div className="card-body">
+            <Bar data={data_ov_analysis2} options={options_ov_analysis2} />  
+          </div>
+        </div>
+      </div>
+    )
+  }
 
   async componentDidMount() {
       
@@ -43,41 +146,65 @@ class contentPage extends React.Component{
     var now_user_location1 = [];
     var in_user_location1 = [];
     var out_user_location1 = [];
+    let accum_now_user1 = 0
+    let accum_in_user1 = 0
+    let accum_out_user1 = 0
 
     var time_location2 = [];
     var now_user_location2 = [];
     var in_user_location2 = [];
     var out_user_location2 = [];
+    let accum_now_user2 = 0
+    let accum_in_user2 = 0
+    let accum_out_user2 = 0
 
     var time_location3 = [];
     var now_user_location3 = [];
     var in_user_location3 = [];
     var out_user_location3 = [];
+    let accum_now_user3 = 0
+    let accum_in_user3 = 0
+    let accum_out_user3 = 0
 
     var time_location4 = [];
     var now_user_location4 = [];
     var in_user_location4 = [];
     var out_user_location4 = [];
+    let accum_now_user4 = 0
+    let accum_in_user4 = 0
+    let accum_out_user4 = 0
 
     var time_location5 = [];
     var now_user_location5 = [];
     var in_user_location5 = [];
     var out_user_location5 = [];
+    let accum_now_user5 = 0
+    let accum_in_user5 = 0
+    let accum_out_user5 = 0
 
     var time_location6 = [];
     var now_user_location6 = [];
     var in_user_location6 = [];
     var out_user_location6 = [];
+    let accum_now_user6 = 0
+    let accum_in_user6 = 0
+    let accum_out_user6 = 0
 
     var time_location7 = [];
     var now_user_location7 = [];
     var in_user_location7 = [];
     var out_user_location7 = [];
+    let accum_now_user7 = 0
+    let accum_in_user7 = 0
+    let accum_out_user7 = 0
 
     var time_location8 = [];
     var now_user_location8 = [];
     var in_user_location8 = [];
     var out_user_location8 = [];
+    let accum_now_user8 = 0
+    let accum_in_user8 = 0
+    let accum_out_user8 = 0
 
     this.state.dataArr.map(value => {
       const time = moment(value.schedule_date).utc().local().format('h:mm a');
@@ -86,49 +213,77 @@ class contentPage extends React.Component{
         now_user_location1.push(value.now_user)
         in_user_location1.push(value.get_in_user)
         out_user_location1.push(value.get_out_user)
+        accum_now_user1 += value.now_user
+        accum_in_user1 += value.get_in_user
+        accum_out_user1 += value.get_out_user
       }
       else if (value.location === 'station2'){
         time_location2.push(time)
         now_user_location2.push(value.now_user)
         in_user_location2.push(value.get_in_user)
         out_user_location2.push(value.get_out_user)
+        accum_now_user2 += value.now_user
+        accum_in_user2 += value.get_in_user
+        accum_out_user2 += value.get_out_user
       }
       else if (value.location === 'station3'){
         time_location3.push(time)
         now_user_location3.push(value.now_user)
         in_user_location3.push(value.get_in_user)
         out_user_location3.push(value.get_out_user)
+        accum_now_user3 += value.now_user
+        accum_in_user3 += value.get_in_user
+        accum_out_user3 += value.get_out_user
       }
       else if (value.location === 'station4'){
         time_location4.push(time)
         now_user_location4.push(value.now_user)
         in_user_location4.push(value.get_in_user)
         out_user_location4.push(value.get_out_user)
+        accum_now_user4 += value.now_user
+        accum_in_user4 += value.get_in_user
+        accum_out_user4 += value.get_out_user
       }
       else if (value.location === 'station5'){
         time_location5.push(time)
         now_user_location5.push(value.now_user)
         in_user_location5.push(value.get_in_user)
         out_user_location5.push(value.get_out_user)
+        accum_now_user5 += value.now_user
+        accum_in_user5 += value.get_in_user
+        accum_out_user5 += value.get_out_user
       }
       else if (value.location === 'station6'){
         time_location6.push(time)
         now_user_location6.push(value.now_user)
         in_user_location6.push(value.get_in_user)
         out_user_location6.push(value.get_out_user)
+        accum_now_user6 += value.now_user
+        accum_in_user6 += value.get_in_user
+        accum_out_user6 += value.get_out_user
       }
       else if (value.location === 'station7'){
         time_location7.push(time)
         now_user_location7.push(value.now_user)
         in_user_location7.push(value.get_in_user)
         out_user_location7.push(value.get_out_user)
+        accum_now_user7 += value.now_user
+        accum_in_user7 += value.get_in_user
+        accum_out_user7 += value.get_out_user
       }
       else if (value.location === 'station8'){
         time_location8.push(time)
         now_user_location8.push(value.now_user)
         in_user_location8.push(value.get_in_user)
         out_user_location8.push(value.get_out_user)
+        accum_now_user8 += value.now_user
+        accum_in_user8 += value.get_in_user
+        accum_out_user8 += value.get_out_user
       }
+      this.setState({dataGraphOv1: [accum_now_user1, accum_now_user2, accum_now_user3, accum_now_user4, accum_now_user5, accum_now_user6, accum_now_user7, accum_now_user8] })
+      this.setState({dataGraphOv2: [accum_in_user1, accum_in_user2, accum_in_user3, accum_in_user4, accum_in_user5, accum_in_user6, accum_in_user7, accum_in_user8] })
+      this.setState({dataGraphOv3: [accum_out_user1, accum_out_user2, accum_out_user3, accum_out_user4, accum_out_user5, accum_out_user6, accum_out_user7, accum_out_user8] })
+
     });
 
         return (
@@ -237,13 +392,35 @@ class contentPage extends React.Component{
                 </div>
               </div>
 
-              <div className="card-header pb-0 p-3">
-                      <div className="d-flex justify-content-between">
-                        <h6 className="mb-2"> Daily Analysis </h6>
-                      </div>
+              <div className="row mt-4">
+              <div className="col-lg-4 "></div>
+                <div className="col-lg-4 mb-lg-0 mb-4">
+                  <div className="card ">
+                    <div className="card-body pb-0 p-3">
+                      <h6 className="mb-2"> Today's Overview Analysis </h6>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="App-bg-graph">  
-                <div className="row mt-4">
+
+              <div className="row mt-4">
+                {this.data_ov1()}
+                {this.data_ov2()}
+              </div>
+
+
+              <div className="row mt-4">
+              <div className="col-lg-4 "></div>
+                <div className="col-lg-4 mb-lg-0 mb-4">
+                  <div className="card ">
+                    <div className="card-body pb-0 p-3">
+                      <h6 className="mb-2"> Today's Station Analysis </h6>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="row mt-4">
                 {this.state.dataArr2.map(value => {
                       var time_location = [];
                       var now_user_location = [];
@@ -299,42 +476,72 @@ class contentPage extends React.Component{
                         out_user_location = out_user_location8;
                       }
 
-                  const data = {
+                  const data_st_analysis = {
                     labels: time_location,
                     datasets: [
                       {
                         label: "Current Passenger",
                         data: now_user_location,
                         fill: true,
-                        backgroundColor: "rgba(75,192,192,0.2)",
-                        borderColor: "rgba(75,192,192,1)"
+                        backgroundColor: "#FF3871",
+                        color: '#FFFFFF'
                       },
                       {
                         label: "Incoming Passenger",
                         data: in_user_location,
-                        fill: false,
-                        borderColor: "#742774"
+                        fill: true,
+                        backgroundColor: "#36FF42",
+                        color: '#FFFFFF'
                       },
                       {
                         label: "Outgoing Passenger",
                         data: out_user_location,
-                        fill: false,
-                        borderColor: "#EF3A4C"
+                        fill: true,
+                        backgroundColor: "#36A0FF",
+                        color: '#FFFFFF'
                       }
+                      
                     ]
+                  };
+                  const options_st_analysis = {
+                    plugins: {  
+                      legend: {
+                        labels: {
+                          color: "white",  
+                          // font: {
+                          //   size: 10
+                          // }
+                        }
+                      }
+                    },
+                    scales: {
+                      y: {
+                        ticks: { color: 'white', beginAtZero: true}
+                      },
+                      x: {
+                        ticks: { color: 'white', beginAtZero: true }
+                      }
+        
+                    }
                   };
 
                   return (
                     <React.Fragment key = {value.location}>
-                          <div className="col-xl-6 col-sm-6">
-                              {value.location}
-                              {/* <canvas id="chart-line" className="chart-canvas" height="300"></canvas> */}
-                              <Line data={data} />  
-                          </div>
+                          <div className="col-xl-3 col-sm-3 mt-3">
+                            <div className="card bg-dark text-white">
+                              <div className="card-body">
+                        
+                                  {value.location}
+                                        <Bar data={data_st_analysis} options={options_st_analysis} />  
+  
+                                </div>
+                      </div>
+                    </div>
+   
                     </React.Fragment>
                   )
                 })}      
-                </div>     
+             
               </div>
 
 
